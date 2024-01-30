71c2e8ca3c54833f8edbbbf6746db3eebad5dd99
Tina Hafezi <tinapayno2@gmail.com>
2024-01-31 01:11:17 +0330

    complete

--- season 1-2/season1notes.py
+++ season 1-2/season1notes.py
@@ -18,7 +18,7 @@ print(e is f)  # it's because they are not stored in the same place
 print(id(e))
 print(id(f))
 
-# binary numbers/ for more examples of binary operators check the sabzlearn-S02E14
+# binary numbers/ for more examples of binary operators check the Sabzlearn-S02E14
 g = 0b0101
 print(bin(a))
 print(g)
