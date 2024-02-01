be383633fd02bd5f1e8bc0b853f3fb75651d9355
Tina Hafezi <tinapayno2@gmail.com>
2024-02-01 19:12:17 +0330

    part two

--- season 3/season 3.py
+++ season 3/season 3.py
@@ -1,4 +1,5 @@
 import fractions
+
 # Data types
 a = {1, 2, 3, 4}  # set
 b = (1, 2, 3, 4)  # tuple
@@ -15,9 +16,9 @@ print(g)  # there is a limit for mantissa in float numbers using scientific nota
 h = 1e-3
 print(h)
 
-i = "c:\Dawnload\\new"  # whenever we want to cancel the effect of something(any character that effects string)
+i = "c:\Dawnload\gew"  # whenever we want to cancel the effect of something(any character that effects string)
 # we use extra backslash
-j =r"c:\Dawnload\new\telegram" # "r" cancels all the special codes in the string such as \n and \t
+j = r"c:\Dawnload\new\telegram"  # "r" cancels all the special codes in the string such as \n and \t
 k = """\
 ti
 sa
@@ -30,3 +31,22 @@ print(k)
 # a simple way for back setting a number
 l = "123456789"
 print(l[::-1])
+
+# string methods
+m = "tina"
+print(len(m))
+print(m.upper())
+print(m.lower())
+print(m.count('n'))
+print(m.endswith('n'))
+print(m.startswith('n'))
+print(m.find('n'))  # return the index of the first character that it found
+print(m.rfind('n'))  # return the index of the last character that it found
+print(m.isalnum())  # are all the characters number and character
+print(m.isnumeric())  # are all the characters number
+n = "tina,sara,paria"
+print(n.split(','))
+print(n.replace(",","+"))
+print(ord("a"))  # the unicode of the character
+print(chr(97))  # the character of the unicode
+print("s: \u0489")  # \u for adding special characters that are not in the keyboard
