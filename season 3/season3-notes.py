ae69386f718b865a57635996bdbf0e1275e05913
Tina Hafezi <tinapayno2@gmail.com>
2024-02-03 23:56:21 +0330

    part 3

--- season 3/season3-notes.py
+++ season 3/season3-notes.py
@@ -17,5 +17,37 @@ h = 1e-3
 print(h)
 
 # a simple way for back setting a number
-l = "123456789"
-print(l[::-1])
+i = "123456789"
+print(i[::-1])
+
+# list
+j = [1, 2, ['a', 3], 4]
+print(j[2][1])
+j.append(['t','g'])
+print(j)
+k, l, *m = [1, 2, 3, 4, 5, 5]
+print(k, ' ', l, ' ', m)
+
+# tuple
+n = (1, 2, 3, 4)  # tuple values cant change but if thr value type is list, we can change the value in the list
+o = 1, 2, 3, 4
+print(type(o))
+
+# dictionary
+p = {"a": 12, "b": [1, 2, 3]}
+print(p["b"])
+p["c"] = 5  # adding new keys and values
+print(p)
+print(p.values())  # return a list of values
+print(p.keys())
+print(p.items())
+del p['b']
+print(p)
+print(sorted(p.values()))
+q = dict([('a', 12), ('b', 23), ('c', 1)])  # dictionary items are tuple so for making a dict we can add tuples
+print(q)
+r ={
+    "1": {"name": 'fd', "age": 23},
+    "2": {"name": 'df', "age": 23}
+}
+print(r)
