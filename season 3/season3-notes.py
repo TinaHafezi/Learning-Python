442976bd5758ac2dc564936051c6ae6f1ab1e1a4
Tina Hafezi <tinapayno2@gmail.com>
2024-02-04 01:11:44 +0330

    complete

--- season 3/season3-notes.py
+++ season 3/season3-notes.py
@@ -23,7 +23,7 @@ print(i[::-1])
 # list
 j = [1, 2, ['a', 3], 4]
 print(j[2][1])
-j.append(['t','g'])
+j.append(['t', 'g'])
 print(j)
 k, l, *m = [1, 2, 3, 4, 5, 5]
 print(k, ' ', l, ' ', m)
@@ -46,8 +46,46 @@ print(p)
 print(sorted(p.values()))
 q = dict([('a', 12), ('b', 23), ('c', 1)])  # dictionary items are tuple so for making a dict we can add tuples
 print(q)
-r ={
+r = {
     "1": {"name": 'fd', "age": 23},
     "2": {"name": 'df', "age": 23}
 }
 print(r)
+s = ["a", "b"]
+t = [20, 12]
+w = dict(zip(s, t))  # zip two list together
+print(w)
+
+# set
+x = {1, 2, 3, 2, 3, 4, 2}  # set do
+print(len(x))
+x.add(6)
+print(x)
+x.remove(6)  # we can remove values that already exit
+x.discard(12)
+x.discard(2)  # but with discard we can remove anything
+print(x)
+y = {1, 2, 5, 67, 34}
+print(x-y)
+print(x.difference(y))
+print(x | y)  # x+y
+print(x.union(y))
+print(x & y)
+print(x.intersection(y))
+print(x ^ y)  # (x|y)-(x&y)
+print(x.symmetric_difference(y))  # (x-y) | (y-x)
+print(x < y)  # is x sub set of y?
+print(x.issubset(y))
+# false: [],(),{},"",set(),none,0 ==> all of these when are empty are considered as false
+print(bool({}))
+print(bool(str("0")))
+# using "and" "or" with just values and not using any compare
+# if(x and y)
+# if x is true==>y
+# if x is false==>x
+print(5 and [])
+# if(x or y)
+# if x is true==>x
+# if x is false==>y
+print(5 or [])
+print(5 or 0 == 6)  # even when we are using a single value and a compression the form is the same
